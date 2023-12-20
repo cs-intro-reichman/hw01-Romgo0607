491a1de6a6c058e6698001a20237d0651275c1d5
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-20 16:41:16 +0200

    update

--- Coins.java
+++ Coins.java
@@ -4,6 +4,12 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int sum = Integer.parseInt(args[0]);
+
+		int coins, quaters;
+		quaters = sum/25;
+		coins = sum%25;
+		
+		System.out.println("Use " + quaters + " quarters, and " + coins + " cents");
 	}
 }
