721645046b10b5532bf87c9040ece8e3da83767d
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-20 18:07:49 +0200

    tringle

--- Triangle.java
+++ Triangle.java
@@ -3,8 +3,24 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
+public class Triangle 
+{
+	public static void main(String[] args) 
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		boolean IsTringle = true;
+
+		int sum1, sum2, sum3;
+		sum1 = a + b;
+		sum2 = a + c;
+		sum3 = b + c;
+
+		if((sum1 < c) || (sum2 <b) || (sum3 < a))		
+			System.out.println(a + ", " + b + ", " + c + ": " + !IsTringle);
+		else 
+			System.out.println(a + ", " + b + ", " + c + ": " + IsTringle);
 	}
 }
