bf220e1e6d03dee4bfbf537a10e4b6284bf65f5f
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-20 19:17:00 +0200

    that took a while

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,49 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
+public class GenThree 
+{
+	public static void main(String[] args) 
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+		int num1, num2, num3;
+		int mini, supermin;
+
+		if(a > b)
+		{
+			a = a - 1;
+			num1 = ((int)(((Math.random() * (a - b + 1) + b))));
+			num2 = ((int)(((Math.random() * (a - b + 1) + b))));
+			num3 = ((int)(((Math.random() * (a - b + 1) + b))));
+
+			System.out.println(num1);
+			System.out.println(num2);
+			System.out.println(num3);
+
+			mini = Math.min(num1, num2);
+			supermin = Math.min(mini, num3);
+
+			System.out.println("The minimal generated number was " + supermin);
+		}
+		else
+		{
+			b = b - 1;
+			num1 = ((int)(((Math.random() * (b - a + 1) + a))));
+			num2 = ((int)(((Math.random() * (b - a + 1) + a))));
+			num3 = ((int)(((Math.random() * (b - a + 1) + a))));
+
+			System.out.println(num1);
+			System.out.println(num2);
+			System.out.println(num3);
+
+			mini = Math.min(num1, num2);
+			supermin = Math.min(mini, num3);
+
+			System.out.println("The minimal generated number was " + supermin);
+		}
+
 	}
 }
+ 
