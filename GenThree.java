7068453518dfb15f2fafa93b073426b0fcbc9d57
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-21 09:35:59 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -9,10 +9,24 @@ public class GenThree
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 
-		int num1, num2, num3;
-		int mini, supermin;
+		int min = Math.min(a, b);
+		int max = Math.max(a, b);
 
-		if(a > b)
+		int num1, num2, num3;
+
+		max = max - 1;
+
+		num1 = ((int)(((Math.random() * (max - min + 1) + min))));
+		num2 = ((int)(((Math.random() * (max - min + 1) + min))));
+		num3 = ((int)(((Math.random() * (max - min + 1) + min))));
+
+		int mini;
+
+		mini = Math.min(num1, num2);
+		supermin = Math.min(mini, num3);
+
+
+		/*if(a > b)
 		{
 			a = a - 1;
 			num1 = ((int)(((Math.random() * (a - b + 1) + b))));
@@ -43,8 +57,7 @@ public class GenThree
 			supermin = Math.min(mini, num3);
 
 			System.out.println("The minimal generated number was " + supermin);
-		}
-
+		} */
 	}
 }
  
